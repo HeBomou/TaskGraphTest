56612ae5198f1a459fc66d412fdb091bba513057
HeBomou <paakmau@outlook.com>
2020-04-09 17:21:56 +0800

    init

---
+++ Source/TaskGraphTest/TankPawn.cpp
@@ -0,0 +1,51 @@
+#include "TankPawn.h"
+
+#include "Components/StaticMeshComponent.h"
+#include "Engine/CollisionProfile.h"
+#include "TaskGraphTestProjectile.h"
+#include "TimerManager.h"
+#include "UObject/ConstructorHelpers.h"
+
+// Sets default values
+ATankPawn::ATankPawn() {
+    // Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+    PrimaryActorTick.bCanEverTick = true;
+
+    // Mesh
+    static ConstructorHelpers::FObjectFinder<UStaticMesh> TankMesh(TEXT("/Game/TwinStick/Meshes/TwinStickUFO.TwinStickUFO"));
+    TankMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TankMesh"));
+    RootComponent = TankMeshComponent;
+    TankMeshComponent->SetCollisionProfileName(UCollisionProfile::Pawn_ProfileName);
+    TankMeshComponent->SetStaticMesh(TankMesh.Object);
+
+    // Fire
+    bCanFire = true;
+}
+
+// Called when the game starts or when spawned
+void ATankPawn::BeginPlay() {
+    Super::BeginPlay();
+}
+
+// Called every frame
+void ATankPawn::Tick(float DeltaTime) {
+    Super::Tick(DeltaTime);
+
+    if (bCanFire) {
+        UWorld* World = GetWorld();
+        if (World) {
+            World->SpawnActor<ATaskGraphTestProjectile>(GetActorLocation() + FVector(90.0f, 0, 0), GetActorRotation());
+        }
+        bCanFire = false;
+        World->GetTimerManager().SetTimer(TimerHandle_LifeSpanExpired, this, &ATankPawn::RecoverWeapon, 0.3f);
+    }
+}
+
+void ATankPawn::RecoverWeapon() {
+    bCanFire = true;
+}
+
+// Called to bind functionality to input
+void ATankPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) {
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
